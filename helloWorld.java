4243d4dbf63ff47d5f67072a8e2165494ebe1096
Shravana Gowri <110216184+shravanagowri@users.noreply.github.com>
2025-05-19 12:11:44 +0530

    Add files via upload

---
+++ helloWorld.java
@@ -0,0 +1,18 @@
+package helloWorld;
+
+public class helloWorld {
+	public static void main(String []args) {
+		int a=10;
+		int b=5;
+		int add=a+b;
+		int subtract=a-b;
+		int mul=a*b;
+		int div=a/b;
+		int modu=a%b;
+				
+System.out.println(add);
+
+		
+	}
+
+}
