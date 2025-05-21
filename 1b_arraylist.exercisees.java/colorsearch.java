632b92e7b61194e3cf0ba5be5484e14d109e7d0e
Shravana Gowri <110216184+shravanagowri@users.noreply.github.com>
2025-05-21 18:54:41 +0530

    colorsearch.java

--- 1b_arraylist.exercisees.java/colorsearch.java
+++ 1b_arraylist.exercisees.java/colorsearch.java
@@ -1,3 +1,5 @@
+/*1b. Write a java program for getting different colors through ArrayList interface and search whether
+the color "Red" is available or not*/
 package ass1b;
 import java.util.ArrayList;
 public class colorsearch {
